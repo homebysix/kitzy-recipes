8647e9b8ae95ae57f6d1ede23fd424bc3522e3b8
Elliot Jordan <elliot@elliotjordan.com>
2020-01-09 23:15:47 -0800

    Use URLGetter superclass, add 1Password 7 feed

--- 1Password 6/OnePasswordURLProvider.py
+++ 1Password 6/OnePasswordURLProvider.py
@@ -4,12 +4,7 @@ from __future__ import absolute_import
 
 import json
 
-from autopkglib import Processor, ProcessorError
-
-try:
-    from urllib.request import urlopen  # For Python 3
-except ImportError:
-    from urllib2 import urlopen  # For Python 2
+from autopkglib import Processor, ProcessorError, URLGetter
 
 __all__ = ["OnePasswordURLProvider"]
 
@@ -19,20 +14,28 @@ __all__ = ["OnePasswordURLProvider"]
 # - String "OPM4"
 # - Locale
 # - The 1Password build number to update from
-UPDATE_URL_FOUR = "https://app-updates.agilebits.com/check/1/13.0.0/OPM4/en/400600"
-UPDATE_URL_FIVE = "https://app-updates.agilebits.com/check/1/14.0.0/OPM4/en/500000"
-UPDATE_URL_SIX = "https://app-updates.agilebits.com/check/1/14.0.0/OPM4/en/553001"
+UPDATE_URLS = {
+    "4": "https://app-updates.agilebits.com/check/1/13.0.0/OPM4/en/400600",
+    "5": "https://app-updates.agilebits.com/check/1/14.0.0/OPM4/en/500000",
+    "6": "https://app-updates.agilebits.com/check/1/14.0.0/OPM4/en/553001",
+    "7": "https://app-updates.agilebits.com/check/1/18.0.0/OPM7/en/700000",
+}
+
 DEFAULT_SOURCE = "Amazon CloudFront"
 DEFAULT_MAJOR_VERSION = "6"
 
 
-class OnePasswordURLProvider(Processor):
+class OnePasswordURLProvider(URLGetter):
     """Provides a download URL for the latest 1Password"""
+
     input_variables = {
         "major_version": {
             "required": False,
-            "description": "The 1Password major version to get. "
-            "Possible values are '4', '5' or '6' and the default is '6'",
+            "description": (
+                "The 1Password major version to get. "
+                "Possible values are '4', '5', '6', or '7' and the default "
+                "is '%s'" % DEFAULT_MAJOR_VERSION
+            ),
         },
         "base_url": {
             "required": False,
@@ -40,27 +43,17 @@ class OnePasswordURLProvider(Processor):
         },
         "source": {
             "required": False,
-            "description": "Where to download the disk image. "
-            "Possible values are 'Amazon CloudFront', 'CacheFly' and 'AgileBits'. "
-            "Default is Amazon CloudFront."
-        }
-    }
-    output_variables = {
-        "url": {
-            "description": "URL to the latest 1Password release.",
+            "description": (
+                "Where to download the disk image. "
+                "Possible values are 'Amazon CloudFront', 'CacheFly' and 'AgileBits'. "
+                "Default is %s." % DEFAULT_SOURCE
+            ),
         },
     }
+    output_variables = {
+        "url": {"description": "URL to the latest 1Password release.",},
+    }
     description = __doc__
-
-    def download_update_info(self, base_url):
-        """Downloads the update url and returns a json object"""
-        try:
-            f = urlopen(base_url)
-            json_data = json.load(f)
-        except Exception as e:
-            raise ProcessorError("Can't download %s: %s" % (base_url, e))
-
-        return json_data
 
     def get_1Password_dmg_url(self, base_url, preferred_source):
         """Find and return a download URL"""
@@ -69,31 +62,32 @@ class OnePasswordURLProvider(Processor):
 
         # 1Password update check gets a JSON response from the server.
         # Grab it and parse...
-        info_plist = self.download_update_info(base_url)
-        version = info_plist.get('version', None)
+        update_data = json.loads(self.download(base_url))
+        version = update_data.get("version", None)
         self.output("Found version %s" % version)
 
-        sources = info_plist.get('sources', [])
-        found_source = next((source for source in sources if source['name'] == preferred_source), None)
-        if found_source:
-            source_url = found_source.get('url', None)
-            if not source_url:
-                raise ProcessorError("No URL found for %s" % preferred_source)
-            return source_url
-        else:
+        sources = update_data.get("sources", [])
+        found_source = next(
+            (src for src in sources if src["name"] == preferred_source), None
+        )
+        if not found_source:
             raise ProcessorError("No download source for %s" % preferred_source)
+
+        source_url = found_source.get("url", None)
+        if not source_url:
+            raise ProcessorError("No URL found for %s" % preferred_source)
+
+        return source_url
 
     def main(self):
         major_version = self.env.get("major_version", DEFAULT_MAJOR_VERSION)
-        if int(major_version) == 4:
-            UPDATE_URL = UPDATE_URL_FOUR
-        elif int(major_version) == 5:
-            UPDATE_URL = UPDATE_URL_FIVE
-        elif int(major_version) == 6:
-            UPDATE_URL = UPDATE_URL_SIX
-        else:
-            raise ProcessorError("Unsupported value for major version: %s" % major_version)
-        base_url = self.env.get("base_url", UPDATE_URL)
+        if major_version not in UPDATE_URLS:
+            raise ProcessorError(
+                "Unsupported value for major version: %s" % major_version
+            )
+
+        update_url = UPDATE_URLS[major_version]
+        base_url = self.env.get("base_url", update_url)
         source = self.env.get("source", DEFAULT_SOURCE)
         self.env["url"] = self.get_1Password_dmg_url(base_url, source)
         self.output("Found URL %s" % self.env["url"])
